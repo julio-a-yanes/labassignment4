83bb8de749482acef2b91d22a211403eb0677243
Angela Carver <angelacarver@protonmail.com>
2020-10-05 16:52:21 -0500

    I added my name to the top of the rc file as a comment.

--- rc.sh
+++ rc.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
+#Angela was here.
 
-#initialize variable
+ #initialize variable
 reverse=""
 
 #read in data
@@ -10,22 +11,27 @@ sequence=`grep -v ">" $1`
 echo "Name: $name"
 
 #calculate length of sequence
-len=${#sequence}
+#len=${#sequence}
 
-echo "Length: $len"
+#echo "Length: $len"
+
+#convert loop into single line which converts to reverse complement
+reverse=`rev $sequence` 
+
 
 #loop through sequence in reverse
-for (( i=$len; i>=0; i-- ))
-do
-    reverse="$reverse${sequence:$i:1}"
-done
+#for (( i=$len; i>=0; i-- ))
+#do
+#    reverse="$reverse${sequence:$i:1}"
+#done
 
 echo "$sequence
 $reverse
 "
 
 #complement sequence
-rc=`echo $reverse | tr 'atcg' 'tagc'`
+#make tr case insensitive
+rc=`echo $reverse | tr 'atcgATCG' 'tagcTAGC'` 
 
 echo "$rc
 "
